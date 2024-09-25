88ff795b57dd7ca49c8cd037c0a339364db9cbe3
Frhan1205 <128057684+Frhan1205@users.noreply.github.com>
2024-09-25 09:21:21 +0700

    Add files via upload

---
+++ sistem.js
@@ -0,0 +1,9 @@
+const nama = document.querySelector('#name');
+const ttl = document.querySelector('#ttl');
+const alamat = document.querySelector('#alamat');
+
+nama.textContent = 'M. FARHAN RIZKILILLAH';
+ttl.textContent = 'Sukabumi, 20 Desember 2005';
+alamat.textContent = 'Kp. Sekarwangi, RT/RW 04/028, Kec Cibadak, Kab Sukabumi';
+
+console.log('File JavaScript berhasil di-load!');
